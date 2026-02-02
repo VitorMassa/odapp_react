a7a47cf8239e3c838dd1b94c7240088b408666a2
Vitor Massafera <vitormassaferapires@yahoo.com>
2026-02-02 14:26:27 -0300

    Quarto commit.

--- src/pages/login.page.tsx
+++ src/pages/login.page.tsx
@@ -2,20 +2,17 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../context/auth-context.context";
-import ModalLoading from "../components/modal_loading.component"
+import ModalLoading from "../components/modal_loading.component";
 
 const Login = () => {
   const navigate = useNavigate();
   const [login, setLogin] = useState("");
   const [senha, setSenha] = useState("");
 
-  const [showAviso, setShowAviso] = useState(false);
-  const msgAviso = "Usuário ou senha incorreta.";
-
   const [showErro, setShowErro] = useState(false);
-  const msgErro = "Erro de sistema. Favor contatar o suporte.";
+  const msgErro = "Algo Deu Errado";
 
-  const[loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
 
   const auth = useContext(AuthContext);
 
@@ -24,21 +21,15 @@ const Login = () => {
   }
 
   const handleLogin = async () => {
-    setLoading(true)
+    setLoading(true);
     const handleLogin = await auth.login(login, senha);
     if (handleLogin == true) {
-      setShowAviso(false);
       setShowErro(false);
       navigate("/home");
-      setLoading(false)
-    } else if (handleLogin == "401") {
-      setShowAviso(true);
-      setShowErro(false);
-      setLoading(false)
+      setLoading(false);
     } else {
-      setShowAviso(false);
       setShowErro(true);
-      setLoading(false)
+      setLoading(false);
     }
   };
 
@@ -46,9 +37,12 @@ const Login = () => {
     <div className=" h-screen bg-white flex justify-center">
       <div className="div-login">
         <div className="w-full">
-          <div className="w-full bg-blue-900 rounded-t flex justify-center">
-
+          <div className="w-full flex justify-center">
+            <label className="text-blue-600 text-4xl font-bold p-6">
+              CAREAPP
+            </label>
           </div>
+          <div className="w-full bg-blue-900 rounded-t flex justify-center"></div>
           <div className=" mx-24 mt-6">
             <label className="label-login">Usuário:</label>
             <input
@@ -78,26 +72,21 @@ const Login = () => {
                 login != "" && senha != "" ? "btn-on-login" : "btn-off-login"
               }
               disabled={login == "" || senha == ""}
-              onClick={(e) => handleLogin()}
+              onClick={() => handleLogin()}
             >
               Entrar
             </button>
           </div>
           <div className="flex justify-center">
-            {(showErro == true && (
+            {showErro == true && (
               <div className=" div-alert bg-red-600">
                 <p>{msgErro}</p>
               </div>
-            )) ||
-              (showAviso == true && (
-                <div className="mt-10 p-3 h-1/6 bg-yellow-500 text-neutral-50 mx-10 border rounded text-center">
-                  <p>{msgAviso}</p>
-                </div>
-              ))}
+            )}
           </div>
         </div>
       </div>
-      <ModalLoading open={loading} title=""/>
+      <ModalLoading open={loading} title="" />
     </div>
   );
 };
